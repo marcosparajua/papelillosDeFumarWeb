79d4e3084f6c944a0c98d4e2f0af30b70163f63d
marcosparajua <71270558+marcosparajua@users.noreply.github.com>
2022-03-30 03:46:54 +0200

    Add files via upload

---
+++ papelillosDeFumarWeb/app.js
@@ -0,0 +1,63 @@
+//Variables for setup
+
+let container;
+let camera;
+let renderer;
+let scene;
+let house;
+
+function init() {
+  container = document.querySelector(".scene");
+
+  //Create scene
+  scene = new THREE.Scene();
+
+  const fov = 500;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.00000000000000000000001;
+  const far = 10000000000000000;
+
+  //Camera setup
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(-1, 4, 7);
+
+  const ambient = new THREE.AmbientLight(0x4040, 2);
+  scene.add(ambient);
+
+  const light = new THREE.DirectionalLight(0xffff, 1);
+  light.position.set(-10, 1, 1);
+  scene.add(light);
+  //Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: false, alpha: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //Load Model
+  let loader = new THREE.GLTFLoader();
+  loader.load("house/shroom.gltf", function(gltf) {
+    scene.add(gltf.scene);
+    house = gltf.scene.children[0];
+    animate();
+  });
+  
+  
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  house.rotation.x += 0.01;
+  renderer.render(scene, camera);
+}
+
+init();
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+
